a4bd3a2afa6e881050fb797e707629e4b579d824
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-27 18:40:38 +0100

    update ex3/ft_finally_block.py

--- ex3/ft_finally_block.py
+++ ex3/ft_finally_block.py
@@ -1,14 +1,19 @@
 def water_plants(plant_list):
+    success = False
     try:
         print("Opening watering system")
         i = 0
         while i < len(plant_list):
             print(f"Watering {plant_list[i]}")
             i += 1
+        success = True
     except Exception:
         print("Error: Cannot water None - invalid plant!")
+        success = False
     finally:
         print("Closing watering system (cleanup)")
+        if success:
+            print("Watering completed successfully!")
 
 
 def test_watering_system():
