625f83cf0d28039e0b73028df942b8fa2052bb7d
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-27 18:02:51 +0100

    update ex2/ft_custom_errors.py

--- ex2/ft_custom_errors.py
+++ ex2/ft_custom_errors.py
@@ -27,11 +27,27 @@ def test_water_error():
         print(f"Caught {WaterError.__name__}:", e)
 
 
+def test_garden_error():
+    try:
+        raise PlantError("The tomato plant is wilting!")
+    except GardenError as e:
+        print("Caught a garden error:", e)
+
+    try:
+        raise WaterError("Not enough water in the tank!")
+    except GardenError as e:
+        print("Caught a garden error:", e)
+
+
 def test_errors():
     print("\nTesting PlantError...")
     test_plant_error()
+
     print("\nTesting WaterError...")
     test_water_error()
+
+    print("\nTesting catching all garden errors...")
+    test_garden_error()
 
     print("\nAll custom error types work correctly!")
 
